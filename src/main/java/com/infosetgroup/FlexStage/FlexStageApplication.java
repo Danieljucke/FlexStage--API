8ba89f8c623425720990f13885758613b48ecbb0
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-09-26 14:04:21 +0100

    first push

---
+++ src/main/java/com/infosetgroup/FlexStage/FlexStageApplication.java
@@ -0,0 +1,13 @@
+package com.infosetgroup.FlexStage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexStageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infosetgroup/FlexStage/FlexStageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosetgroup.FlexStage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexStageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
