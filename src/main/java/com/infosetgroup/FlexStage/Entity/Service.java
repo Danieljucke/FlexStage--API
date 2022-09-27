e865433cea3a792dd76284cd9cc51f9f7003c390
Joy Daniel <88509670+Danieljucke@users.noreply.github.com>
2022-09-27 09:40:07 +0100

    '#'

--- src/main/java/com/infosetgroup/FlexStage/Entity/Service.java
+++ src/main/java/com/infosetgroup/FlexStage/Entity/Service.java
@@ -8,15 +8,14 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
+@Getter // permet de créer les getters pour toutes les variables que l'on aura dans notre classe
+@Setter // permet de créer les setters pour toutes les variables que l'on aura dans notre classe
+@AllArgsConstructor // permet de créer les constructeurs
+@NoArgsConstructor // permet de créer les constructeurs empty
+@Entity // permet de dire que la classe que l'on implemente est une entité
 public class Service {
-    @Id
+    @Id // permet de dire que la variable id est une clé primaire
     private int id;
     private String nom_service;
     private String code_service;
-
 }
--- src/main/java/com/infosetgroup/FlexStage/Repository/ServiceRepository.java
+++ src/main/java/com/infosetgroup/FlexStage/Repository/ServiceRepository.java
@@ -3,6 +3,7 @@ package com.infosetgroup.FlexStage.Repository;
 import com.infosetgroup.FlexStage.Entity.Service;
 import org.springframework.data.repository.CrudRepository;
 
+//CrudRepository permet de dire que ce repository fera la création, la lecture, la mise a jour et la suppression
 public interface ServiceRepository extends CrudRepository<Service,Integer> {
 
 }
